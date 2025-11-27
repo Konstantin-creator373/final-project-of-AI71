f33628e164bae68efa93a230772095422c823df8
Konstantin-creator373 <filippovkst@yandex.ru>
2025-11-27 17:51:40 +0200

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import re
 import asyncio
-import json
 
-user_data = {}
+user_data = {} #создаём список для данных пользователя
 
-async def phone_number():
+async def phone_number(): #Функция запроса и проверки номера телефона
     phone = input('ведите номер телефона:')
     check_phone = re.search(r'7\s\d{10}', phone)
     await asyncio.sleep(2)
@@ -16,8 +15,8 @@ async def phone_number():
         print('Неправильно введено имя')
         await phone_number()
 
-async def fio():
-    choose_fio = input('Введите свои ФИО: ')
+async def fio(): #Функция для запроса и проверки ФИО
+    choose_fio = input('Введите свои ФИО: ') 
     check_fio = re.search(r'\D{1,999}\s\D{1,999}\s\D{1,999}', choose_fio)
     await asyncio.sleep(2)
 
@@ -28,7 +27,7 @@ async def fio():
         print('Неправильно введено имя')
         await fio()
 
-async def check_old():
+async def check_old(): #Функция проверки возраста
     age = int(input('Введите свой возраст'))
     if age <= 10:
         user_data['age'] = age
@@ -36,7 +35,7 @@ async def check_old():
     else:
         await old_menu()
 
-async def old_menu():
+async def old_menu(): #Меню для взрослых любителей пиццы
     print('Вы выбрали меню для взрослого любителя пиццы')
     print("У нас стартовала акция кастомная пицца. Напишите ингредиенты для пиццы через запятую.")
     print('Вы можете выбрать ингридиенты такие как: Пепперони, Сыр, Кетчуп, Майонез, Лосось, Секретный ингридиент')
@@ -52,7 +51,7 @@ async def old_menu():
         await old_menu()
 
 
-async def new_menu():
+async def new_menu(): #Меню для маленьких любителей пиццы
     print('Вы выбрали меню для маленького любителя пиццы')
     print("У нас стартовала акция кастомная пицца. Напишите ингредиенты для пиццы через запятую.")
     print('Вы можете выбрать ингридиенты такие как: Пепперони, Сыр, Мёртвый цыплёнок, Живой цыплёнок, Те кто улыбается в уникуме, Кетчуп, Майонез')
@@ -68,7 +67,7 @@ async def new_menu():
         await new_menu()
 
 
-async def mail():
+async def mail(): #Функция проверки почты
     choose_mail = input('Введите свой емейл: ')
     check_mail = re.search(r'.{1,999}@.{1,999}ru', choose_mail)
     await asyncio.sleep(2)
@@ -80,9 +79,9 @@ async def mail():
         print('Неправильно введена почта')
         await mail()
 
-async def main():
+async def main(): #Главная функция для запуска других
     await phone_number()
-    print("\n" + "=" * 50 + "\n")
+    print("\n" + "*" * 50 + "\n")
 
 async def save_data():
     with open('user_data.txt', 'a', encoding='utf-8') as file:
@@ -91,14 +90,14 @@ async def save_data():
         for i, j in user_data.items():
             file.write(f'{i}: {j}\n')
 
-async def prepare_order():
+async def prepare_order(): #Готовка заказа
     print('Заказ готовится')
     await asyncio.sleep(4)
     print('Всё готово! С вашей карты было снято 999 рублей! Приходите ещё!')
     await save_data()
 
 
-async def order():
+async def order(): #Подготовка к заказу (запрос даты)
     data = input('Введите свою дату рождения: Пример: 66/66/6666: ')
     check_data = re.search(r'\d{2}/\d{2}/\d{4}', data)
 
@@ -110,4 +109,5 @@ async def order():
         await order()
 
 
-asyncio.run(main())
+asyncio.run(main()) #Запуск главной функции
+
